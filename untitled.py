1c4c514018b5d726a1cff55a1d72c5a80f3075b0
Aiden Gu <aidengu@Aidens-MBP.lan>
2021-06-21 16:52:46 +0800

    relativistic mass class

--- untitled.py
+++ untitled.py
@@ -1,1 +1,14 @@
-print("lmao")
+import numpy as np
+c = 300000000
+
+class spaceship:
+	def __init__(self,mass):
+		self.mass=mass
+	def rel_mass(self,v):
+		m0=v**2/c**2
+		m0=1-m0
+		m0=np.sqrt(m0)
+		return(self.mass/m0)
+
+Apollo13 = spaceship(100)
+print(Apollo13.rel_mass(0.99*c))
